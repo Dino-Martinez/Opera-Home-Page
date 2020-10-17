69b153b647eff1dc6c0200321edeb55c4d0ead3b
Dino-Martinez <dino.martinez.514@gmail.com>
2020-10-16 22:19:09 -0700

    added omnibox functionality

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
 let searchForm = document.getElementById("search-form");
 let submitButton = document.getElementById("submit-form");
 let googleButton = document.getElementById("send-to-google");
-let weather = document.getElementById("weather-object");
-window.addEventListener('load', function() {
-  console.log(weather.childNodes);
+let queryBox = document.getElementById("google-query");
+
+queryBox.addEventListener("keydown", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    submitButton.click();
+  }
 });
 submitButton.addEventListener("click", function() {
-  searchForm.submit();
+  const regex = /^(https?:\/\/)?(www\.)?\w+((\.com)|(\.org)|(\.net)|(\.io))$/g;
+  let searchQuery = queryBox.value;
+  if(searchQuery.match(regex)) {
+    if (!searchQuery.startsWith('https://') && !searchQuery.startsWith('http://')) {
+      searchQuery = "https://" + searchQuery;
+    }
+    window.location.href = searchQuery;
+  } else {
+    searchForm.submit();
+  }
 });
 
 googleButton.addEventListener("click", function() {
-  window.location.href = "http://www.google.com";
+   window.location.href = "http://www.google.com";
 });
