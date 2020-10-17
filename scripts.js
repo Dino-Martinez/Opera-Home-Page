af2cd4f55b38daacaf7d43bea9dacd17a7642cb6
Dino-Martinez <dino.martinez.514@gmail.com>
2020-10-17 09:43:52 -0700

    added more quick links

--- scripts.js
+++ scripts.js
@@ -4,11 +4,8 @@ let googleButton = document.getElementById("send-to-google");
 let queryBox = document.getElementById("google-query");
 console.log("Ready");
 queryBox.addEventListener("keydown", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     submitButton.click();
   }
 });
